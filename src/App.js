c3ca9bc2a80c5aa2b1c650c9587ad5c66b477cda
Said-laasri <saidlaasri3@gmail.com>
2023-02-08 16:22:06 +0100

    Store signed in in localstorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -10,12 +10,22 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("isLoggedIn");
+    if ((storedData === "1")) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   return (
     <React.Fragment>
