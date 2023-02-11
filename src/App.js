bb3f9ae42de2d0da284bed645dd507902ac673c6
Said-laasri <saidlaasri3@gmail.com>
2023-02-11 16:33:34 +0100

    Use the context provider to pass the islogin property to mainheader and navigation without props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import Authcontext from "./components/store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -22,19 +23,19 @@ function App() {
 
   useEffect(() => {
     const storedData = localStorage.getItem("isLoggedIn");
-    if ((storedData === "1")) {
+    if (storedData === "1") {
       setIsLoggedIn(true);
     }
   }, []);
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <Authcontext.Provider value={{ isLoggedIn: isLoggedIn, }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </Authcontext.Provider>
   );
 }
 
