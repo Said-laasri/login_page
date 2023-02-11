e9f8cf266712ee01e29c47799aba110bcad7ede6
Said-laasri <saidlaasri3@gmail.com>
2023-02-11 16:31:53 +0100

    Use hook useContext to assign ctx object the Authcontext value

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,22 +1,24 @@
-import React from 'react';
-
-import classes from './Navigation.module.css';
+import React, { useContext } from "react";
+import Authcontext from "../store/auth-context";
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(Authcontext);
+
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
