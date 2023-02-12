f62751ad4594f79964b62fcdf7cd0f43b915fd26
Said-laasri <saidlaasri3@gmail.com>
2023-02-12 22:04:36 +0100

    Add useContext hook to access the onlogout methode

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Authcontext from '../store/auth-context';
 import Button from '../UI/Button/Button';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
-const Home = (props) => {
+const Home = () => {
+  const autConx = useContext(Authcontext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>Logout</Button>
+      <Button onClick={autConx.onLogout}>Logout</Button>
     </Card>
   );
 };
