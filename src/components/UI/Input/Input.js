2d5a1241607e4bc0ecb7daa1bb94941a267698da
Said-laasri <saidlaasri3@gmail.com>
2023-02-17 21:55:23 +0100

    Add focus to input on empty

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from "react";
 
-import classes from'./Input.module.css'
+import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
 
-    return (
-        <div
-        className={`${classes.control} ${
-          props.isValid === false ? classes.invalid : ""
-        }`}
-      >
-        <label htmlFor={props.id}>{props.label}</label>
-        <input
-          type={props.type}
-          id={props.id}
-          value={props.value}
-          onChange={props.onChange}
-          onBlur={props.onBlur}
-        />
-      </div>
-    )
-};
-        
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        type={props.type}
+        id={props.id}
+        ref={inputRef}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
 export default Input;
